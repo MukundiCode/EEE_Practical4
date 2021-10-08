4138b7dd7b70684175fe3c500ece52b3b5328f82
Mukundi <tinashechitamba@outlook.com>
2021-10-08 16:59:14 +0100

    sort of working

--- adc.py
+++ adc.py
@@ -3,6 +3,7 @@ import digitalio
 import board 
 import adafruit_mcp3xxx.mcp3008 as MCP 
 from adafruit_mcp3xxx.analog_in import AnalogIn
+from adafruit_debouncer import Debouncer
 
 import threading
 import time
@@ -16,26 +17,31 @@ MISO = 21
 MOSI = 19
 CE0 = 24
 sampling = [10, 5, 1] #different time intervals
-global i
-i = 0 #sampling index for different time intervals
+#global i
+#i = 0 #sampling index for different time intervals
 
 #setting up button
-BTN = digitalio.DigitalInOut(board.D7)
-BTN.direction = digitalio.Direction.INPUT
-BTN.pull = digitalio.Pull.DOWN
+BTN = 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def change_sample(a):
-	if a == 2:
-		i = 0
+def change_sample(x):
+	if x == 2:
+		x = 0
 	else:
-		i= i+1
-	print("Button Pressed")
+		x= x+1
+	print("Sampling updated to",sampling[x],"seconds")
+	return x
 
 def read_adc():
-	global spi, cs, mcp, chan, chan1, chan3 
+	global spi, cs, mcp, chan2, chan1, chan3, button ,switch
 	# create the spi bus
 	spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-
+	
+	button = digitalio.DigitalInOut(board.D23)
+	button.direction = digitalio.Direction.INPUT
+	button.pull = digitalio.Pull.UP
+	switch = Debouncer(button,interval=0.1)
 	# create the cs (chip select)
 	cs = digitalio.DigitalInOut(board.D5)
 
@@ -43,29 +49,19 @@ def read_adc():
 	mcp = MCP.MCP3008(spi, cs)
 
 	# create an analog input channel on pin 0
-	chan = AnalogIn(mcp, MCP.P2)
+	chan2 = AnalogIn(mcp, MCP.P2)
 
 	# create analog input channel on pin 1
 	chan1 = AnalogIn(mcp, MCP.P1)
 
-	#attempt at creating interrupt
-	chan3 = AnalogIn(mcp, MCP.P3)
-
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(BTN, GPIO.OUT)
-#GPIO.output(BTN, GPIO.HIGH)
-#GPIO.add_event_detect(BTN, GPIO.RISING, callback= change_sample, bouncetime=300)
-
-#attempt at implementing interrupt
-def my_callback():
-	""" 
-	THIS CHANGES THE SAMPLING INTERVALS; 10s, 5s, 1s
-	"""
-	print("Button pressed!!")
 
 #GPIO.add_event_detect(BTN, GPIO.FALLING, callback=my_callback, bouncetime=300)
 
+"""def setup():
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	#GPIO.add_event_detect(BTN, GPIO.FALLING, callback=my_callback, bouncetime=300)
+"""
 
 def sensor_temp(adc_value):
 	"""Temperature calculation"""
@@ -81,14 +77,35 @@ def sensor_temp(adc_value):
 print("{:<15} {:<15} {:<15} {:>2} {:<15}".format('Runtime','Temp Reading','Temp',"",'Light Reading'))
 start = time.time()
 
-while True:
-	x = threading.Thread(target=read_adc)
-	x.start()
-	x.join()
-	end = time.time() 
-	print("{:<15} {:<15} {:<15.1f} {:>2} {:<15}".format(str(math.floor((end-start)))+"s", chan.value,sensor_temp(chan.value), "C", chan1.value))
-	print(BTN.value)
-	if BTN.value == False:
-		change_sample(i)
-	time.sleep(sampling[i])
-	pass
+
+def print_time_thread():
+    """
+    This function prints the time to the screen every five seconds
+    """
+    thread = threading.Timer(sampling[i], print_time_thread)
+    thread.daemon = True  # Daemon threads exit when the program does
+    thread.start()
+    read_adc()
+    #print(i)
+    print("{:<15} {:<15} {:<15.1f} {:>2} {:<15}".format(str(math.floor((time.time()-start)))+"s", chan1.value,sensor_temp(chan1.value), "C", chan2.value))
+    #print(datetime.datetime.now())
+
+
+if __name__ == "__main__":
+    global i 
+    i = 0
+    try:
+        start = time.time()
+        print_time_thread() # call it once to start the thread
+        #setup()
+        # Tell our program to run indefinitely
+        while True:
+            switch.update()
+            #GPIO.add_event_detect(BTN, GPIO.FALLING, callback=my_callback, bouncetime=300)
+            if  switch.rose:
+                i = change_sample(i)
+            pass
+    except Exception as e:
+        print(e)
+    finally:
+        GPIO.cleanup()
